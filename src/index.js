3968a930799604df1b387ca8f10b008052bbb2b3
sunilmsz <inpb91@gmail.com>
2022-07-11 16:39:03 +0530

    static json added

--- src/index.js
+++ src/index.js
@@ -76,6 +76,10 @@ io.on("connection",(socket)=> {
 
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"))
+    const path= require("path")
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
 
